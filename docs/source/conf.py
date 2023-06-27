c0f0f1fbb8eca439f095bc9ec561fa9c1f37d0b9
Ari <ari@cogniteam.com>
2023-06-27 11:07:10 +0300

    test dark mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,10 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_dark_mode'
+
+# user starts in dark mode
+default_dark_mode = True
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
