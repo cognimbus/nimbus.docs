921df170a70ba12e3907494f3ce32d532ee103ec
Ari Yakir <106956349+AriYakir@users.noreply.github.com>
2024-06-30 09:53:21 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ version = '0.1.0'
 master_doc = 'index'
 
 # -- General configuration
-htmltitle = "Cogniteam Cloud Platform"
+html_title = "Cogniteam Cloud Platform"
 
 html_theme_options = {
     'logo_only': True,
